19d1e45ae4d300a7c654562f82854e0fbbf6e665
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-27 15:15:38 +0530

    create background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App" style={{backgroundImage: "url(./ques1.png)"}}>
+    
+  </div>;
 }
 
 export default App;

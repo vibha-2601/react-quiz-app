8faaa091731411c0a7d0a9ae2f15c245710ffe31
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-27 15:55:36 +0530

    create footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Header />
     
   </div>
+  <Footer />
   </BrowserRouter>
   
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div style={{ textAlign: "center", marginBottom: 10 }}>
+      Made by <a style={{ cursor: "pointer" }}>Vibha Nimbalkar</a>
+    </div>
+  );
+};
+
+export default Footer;

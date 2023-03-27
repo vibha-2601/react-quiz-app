c622f1b62989b548dc0e3cc96585a654c4ab554b
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-27 16:32:57 +0530

    create Pages folder structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
+
+
 
 function App() {
-  return ( 
-  <BrowserRouter>
-  <div className="App" style={{backgroundImage: "url(./ques1.png)"}}>
-    <Header />
-    
-  </div>
-  <Footer />
-  </BrowserRouter>
-  
+  return (
+    <Router>
+      <div className="App" style={{ backgroundImage: "url(./ques1.png)" }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/quiz' element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 

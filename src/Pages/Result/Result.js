2fc6f7f7a99dc81be0a08f9f025ffcd991c1f1c2
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-29 20:06:17 +0530

    create Result page functionality

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,32 +1,31 @@
-import { Button } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Result.css';
+import { Button } from "@mui/material";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Result.css";
 
 const Result = ({ name, score }) => {
-
   const navigate = useNavigate();
 
   useEffect(() => {
-if (!name) {
-  navigate("/")
-}
-  }, [name, navigate])
+    if (!name) {
+      navigate("/");
+    }
+  }, [name, navigate]);
 
   return (
-    <div className='result'>
-      <span className='title'>Final Score: {score}</span>
+    <div className="result">
+      <span className="title">Final Score: {score}</span>
       <Button
-      variant="contained"
-      color="secondary"
-      size="large"
-      style={{ alignSelf: 'center', marginTop: 20 }}
-      href="/"
+        variant="contained"
+        color="secondary"
+        size="large"
+        style={{ alignSelf: "center", marginTop: 20 }}
+        href="/"
       >
         Go to Homepage
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
